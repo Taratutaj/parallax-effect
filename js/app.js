2f2a1d1e4563855ee4adf59a0399514cc134c10a
Joanna Taratuta <joannataratuta@gmail.com>
2024-02-01 16:48:55 +0100

    Add Gsap animation

--- js/app.js
+++ js/app.js
@@ -13,9 +13,9 @@ function update(cursorPosition) {
     let speedx = el.dataset.speedx;
     let speedy = el.dataset.speedy;
 
-    el.style.transform = `translateX(calc(-50% + ${
+    el.style.transform = `translateX(calc( ${
       -xValue * speedx
-    }px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+    }px)) translateY(calc( ${yValue * speedy}px))`;
   });
 }
 update(0);
@@ -29,7 +29,20 @@ window.addEventListener("mousemove", (e) => {
 /* GSAP Animation */
 
 let timeline = gsap.timeline();
-timeline.from(".bg-img", {
-  top: `${document.querySelector(".bg-img").offsetHeight / 2}px`,
-  duration: 0,
-});
+
+timeline.from(".bg-img", { y: `100px`, duration: 1 }, 0);
+timeline.from(".moon", { y: `400px`, duration: 1 }, 0);
+timeline.from(".planet", { y: `300px`, duration: 1 }, 0);
+timeline.from(".cloud_1", { y: `200px`, duration: 1 }, 0.5);
+timeline.from(".cloud_2", { y: `300px`, duration: 1 }, 0.5);
+timeline.from(".building_1", { y: `300px`, duration: 2 }, 0.6);
+timeline.from(".building_2", { y: `400px`, duration: 2 }, 0.7);
+timeline.from(".fog_1", { y: `400px`, duration: 2 }, 0.8);
+timeline.from(".spaceship", { x: `500px`, duration: 2 }, 0.8);
+timeline.from(".building_3", { y: `500px`, duration: 3 }, 0.9);
+timeline.from(".fog_2", { y: `500px`, duration: 3 }, 1);
+timeline.from(".building_4", { y: `500px`, duration: 3 }, 1);
+timeline.from(".building_5", { y: `600px`, duration: 3 }, 1);
+timeline.from(".fog_3", { y: `600px`, duration: 3 }, 1);
+timeline.from(".rocks_1", { y: `700px`, duration: 3 }, 1);
+timeline.from(".rocks_2", { y: `700px`, duration: 2 }, 1);
